4079ee53e16ac8e540e5f7910de13229d567be22
Yuri Tamai <yuri@mbp16.ir.his.u-fukui.ac.jp>
2018-08-06 17:41:56 +0900

    add new file

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+})
